29114d1d38ac4fec3629b2c283caf5514fc3261e
Syphon <siddiquishifa78@gmail.com>
2025-10-06 12:38:28 +0530

    binding succesfull here
    binding done
    client connects
    no messages propogation

--- TCP_UDP/clientTCP.c
+++ TCP_UDP/clientTCP.c
@@ -1,23 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
 
 int main(void) 
 {
-	
-	int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(2000);
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	char server_message[2000], client_message[2000];
 	
-	//send the req to server 
-	connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	
-	//it'll wait for server to recv()
+	int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if (socket_desc < 0){
+          printf("[-]Socket error");
+          exit(1);
+        }
+        printf("[+]TCP server socket created.\n");
+	
+	connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	printf("connected to the server \n");
+	      
+	//send the req to server and wait for server to recv()
 	client_message[2000] = "Hello from client";
   	server_message[2000] = "Hello from server";
   	
--- TCP_UDP/serverTCP.c
+++ TCP_UDP/serverTCP.c
@@ -6,8 +6,20 @@
 
 int main(void)
 {
+
+  int socket_desc;
+  int client_sock; //waits at accept
+  
+  //server keeps address of server & client in server_addr
+  struct sockaddr_in server_addr, client_addr;
+  
+  
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(2000); //local PORT
+  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //local IP
+  
   ///////////////creating a AF_INET (ipv4) internet protocol. STOCK_STEAM = TCP connections. 0 = choose any appropriate protocol
-  int socket_desc = socket(AF_INET, SOCK_STREAM, 0); //(domain,type,protocol)
+  socket_desc = socket(AF_INET, SOCK_STREAM, 0);
   
   //check socket connection
   if (socket_desc == -1) { 
@@ -17,25 +29,18 @@ int main(void)
     else
         printf("Socket successfully created...\n"); 
   
-  //server keeps address of server & client in server_addr
-  struct sockaddr_in server_addr;
-  
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(2000); //local PORT
-  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //local IP
-  
   /////////////////////bind assigns a local port address to the socket
-  bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  int n = bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr));
   
   //check binding
-  if ((bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr))) != 0) { 
+  if (n != 0) { 
       printf("socket bind failed...\n"); 
       exit(0); 
   } 
   else
       printf("Socket successfully binded...\n"); 
   
-  //listen to req
+  ///////////////////////listen to req
   listen(socket_desc, 1);
   
   //check listen
@@ -45,30 +50,24 @@ int main(void)
   }
   printf("\nListening for incoming connections.....\n");
   
-  //store the clients address
-  struct sockaddr client_addr;
-  int client_size = sizeof(client_addr);
   
   //wait at accept until client connects
-  int client_sock = accept(socket_desc, (struct sockaddr*)&client_addr, &client_size);
+  int client_size = sizeof(client_addr);
+  client_sock = accept(socket_desc, (struct sockaddr*)&client_addr, &client_size);
   
-  /*
   if (client_sock < 0){
     printf("Can't accept\n");
     return -1;
   }
-  printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port))
-  */
+  printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
   
   //wait at recieve
-  char client_message[2000] = "Hello from client";
-  char server_message[2000] = "Hello from server";
+  char client_message[2000] = "Hello i am server file, sending client msg";
+  char server_message[2000] = "Hello i am server file, sending server msg";
   
   recv(client_sock, client_message, sizeof(client_message), 0);
   send(client_sock, server_message, strlen(server_message), 0);
   
-  close(client_sock);
-  close(socket_desc);
 
 
 }
